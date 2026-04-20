f2462180d6f228b2a740f3a7a52acdea084d0730
preetham-nandyala <nandyala.preetham755@gmail.com>
2026-04-20 12:22:34 +0530

    final commit

--- test.js
+++
@@ -1,7 +0,0 @@
-fetch("http://localhost:11434/api/generate", {
-    method: "POST",
-    headers: {"Content-Type": "application/json"},
-    body: JSON.stringify({model: "qwen2.5:0.5b", prompt: "Hello"})
-}).then(async res => {
-    console.log(res.status, await res.text());
-}).catch(console.error);
